1d86c1d4a8120696ffb89d1ba2fe175a256cc5bf
kkaoww <120167981+kkaoww@users.noreply.github.com>
2023-01-17 16:06:55 +0700

    Update lab15_1.cpp

--- lab15_1.cpp
+++ lab15_1.cpp
@@ -4,8 +4,22 @@ using namespace std;
 template <typename T>
 void insertionSort(T d[],int N){
 
+    int j;
+    for( int i = 1; i < N; i++)
+    {
+        T temp = d[i];
+        j = i - 1;
+        cout << "Pass " << i << ":";
+        while(j >= 0 && d[j] < temp)
+        {
+            d[j + 1] = d[j];
+            j--;
+        }
+        d[j + 1] = temp;
+        for (int i = 0; i < N; i++) cout << d[i] << " ";
+        cout << endl;
+ }
 }
-
 int main(){
 	int a[10] = {12,25,30,44,2,0,4,7,55,25};
 	cout << "Input Array:";
