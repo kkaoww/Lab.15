26bfc84b6b71f07cd92b0929e21ef23872e3f9f8
kkaoww <120167981+kkaoww@users.noreply.github.com>
2023-01-17 17:13:14 +0700

    Update lab15_2.cpp

--- lab15_2.cpp
+++ lab15_2.cpp
@@ -3,11 +3,42 @@ using namespace std;
 
 const int N = 5;
 
-void inputMatrix(double [][N]);
+void inputMatrix(double t[][N]){
+    for(int i = 0; i < N; i++){
+    cout << "Row " << i + 1 << ": ";
+        for(int j = 0; j < N; j++){
+         cin >> t[i][j] ;
+        }
+    }
+}
 
-void findLocalMax(const double [][N], bool [][N]);
+void findLocalMax(const double t[][N], bool k[][N])
+{
+	for(int i = 0; i < N; i++)
+	{
+		for(int j = 0; j < N; j++)
+        {
+			if ((i > 0 && i <= N - 2) && (j > 0 && j <= N - 2))
+			{
+				if ((t[i][j] >= t[i - 1][j]) && (t[i][j] >= t[i][j - 1]) && (t[i][j] >= t[i][j + 1]) && (t[i][j] >= t[i + 1][j]))
+				{
+					k[i][j] = 1;
+				}
+				else k[i][j] = 0;
+			}
+			else k[i][j] = 0;
+		}
+	}
+}
 
-void showMatrix(const bool [][N]);
+void showMatrix(const bool t[][N]){
+ for(int i = 0; i < N; ++i){
+    for(int j = 0; j < N; j++){
+    cout << t[i][j] << " " ;
+      }
+  cout << endl;
+  }
+}
 
 int main(){
 	double A[N][N]; 
